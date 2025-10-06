afb01c36a3fcc7ef47b0c7d9601fe4b0584461fe
kyoya <taka.1999.0804@gmail.com>
2025-10-06 20:50:13 +0900

    10/06途中

--- Assets/Undead Survivor/Scripts/Inventory.cs
+++ Assets/Undead Survivor/Scripts/Inventory.cs
@@ -7,6 +7,10 @@ public class Inventory : MonoBehaviour
     // 武器の種類と個数を管理するDictionary
     public Dictionary<WeaponData, int> weapons = new Dictionary<WeaponData, int>();
 
+    GameObject sup1;
+    GameObject sup2;
+    GameObject sup3;
+
     // インベントリUIのアイコンImageの配列
     [SerializeField] private Image[] itemIcons;
     // アイテムの個数を表示するテキストの配列
@@ -28,8 +32,14 @@ public class Inventory : MonoBehaviour
             Debug.Log(newWeapon.weaponName + "をインベントリに追加しました。現在の個数: " + weapons[newWeapon]);
         }
         UIUpdater();
+
+        if (newWeapon.weaponName == "Supporter1")
+        {
+            sup1 = GameObject.Find("Supporter1");
+            sup1.SetActive(true);
+        }
     }
-    
+
     // 武器を削除するメソッド（必要に応じて実装）
     public void RemoveWeapon(WeaponData weaponToRemove)
     {
@@ -56,7 +66,7 @@ public class Inventory : MonoBehaviour
             {
                 // UIに表示する武器のデータ
                 WeaponData weapon = uniqueWeapons[i];
-                
+
                 itemIcons[i].sprite = weapon.icon; // アイコン画像を設定
                 itemIcons[i].enabled = true; // 画像を表示
             }
