9d4de512cc62790d3666a999ab1ce8b0e0d3c8d4
hogehoge <19991118mmm@gmail.com>
2025-10-08 21:44:38 +0900

    GAMEOVER時の処理を追加

--- Assets/Undead Survivor/Scripts/Wall.cs
+++ Assets/Undead Survivor/Scripts/Wall.cs
@@ -1,15 +1,37 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Wall : MonoBehaviour
 {
+    public GameObject GameOverCanvas; //gameover表示オブジェクト
+    public GameObject IsRetryCanvas; //continue?表示オブジェクト
+
     public static Wall Instance { get; private set; }
     AudioSource destroySound;    //バリケード崩壊音
     public float WallHp { get; private set; }
     public float WallMaxHp { get; private set; }
 
 
+    public void OnRetryYes() //リトライする
+    {
+        Time.timeScale = 1f; // ポーズ解除
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // 現在のシーンを再読み込み
+    }
+
+    public void OnRetryNo() //リトライしない
+    {
+        Time.timeScale = 1f; // ポーズ解除
+        SceneManager.LoadScene("StartScene"); // タイトル画面など別シーン名に変更
+    }
+
+
     private void Awake()
     {
+        //gameoverは最初非表示
+        IsRetryCanvas.SetActive(false);
+        GameOverCanvas.SetActive(false);
+
         //死んだ音
         destroySound = GetComponent<AudioSource>();
 
@@ -19,6 +41,8 @@ public class Wall : MonoBehaviour
             DontDestroyOnLoad(gameObject);
             WallMaxHp = 100;
             WallHp = WallMaxHp;
+            Debug.Log(WallHp);
+            
         }
         else
         {
@@ -39,6 +63,15 @@ public class Wall : MonoBehaviour
             Debug.Log("ゲームオーバー");
             // ゲームオーバー処理をここに記述
             // GameManager.Instance.GameOver();
+
+            //gameoverCanvasを表示する
+            GameOverCanvas.SetActive(true);
+
+            //ゲームをポーズする
+            Time.timeScale = 0f;
+
+            //continue?
+            IsRetryCanvas.SetActive(true);
         }
     }
 
