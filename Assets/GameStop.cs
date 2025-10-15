990187d87445d0d90c00d1f7f1fa76cbcdebc3e1
hogehoge <19991118mmm@gmail.com>
2025-10-15 19:38:29 +0900

    サウンドいろいろ追加

--- Assets/GameStop.cs
+++ Assets/GameStop.cs
@@ -1,3 +1,4 @@
+using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,10 +6,37 @@ public class GameStop : MonoBehaviour
 {
     public GameObject pauseCanvas; // ポーズメニュー用キャンバス
     public GameObject stopButton;  // ストップボタン（再表示用）
+    AudioSource stop; //ストップボタンのオーディオソース
+    public AudioClip stopSound;  //ストップボタンを押したときの音
+    public AudioClip closeSound; //再開＆タイトルへ戻るボタンを押したときの音
+
+    void Start()
+    {
+        //Button btn = GetComponent<Button>();
+        //btn.onClick.AddListener(PlayClickSound);
+        stop = GetComponent<AudioSource>();
+        if (stop == null)
+        {
+            Debug.Log("null");
+
+        }
+        else
+        {
+            Debug.Log("not null");
+        }
+        Debug.Break();
+    }
+
+    void PlayClickSound()
+    {
+        //stop.PlayOneShot(clickSound);
+    }
 
     // ストップボタンを押したとき
     public void OpenStopButton()
     {
+        stop.clip = stopSound;
+        stop.Play();
         Time.timeScale = 0f; // ゲームを一時停止
         pauseCanvas.SetActive(true); // ポーズキャンバス表示
         stopButton.SetActive(false); // ストップボタンは非表示
@@ -17,6 +45,8 @@ public class GameStop : MonoBehaviour
     // 再開ボタンを押したとき
     public void OnRetryYes()
     {
+        stop.clip = closeSound;
+        stop.Play();
         Time.timeScale = 1f; // ポーズ解除
         pauseCanvas.SetActive(false); // ポーズキャンバス非表示
         stopButton.SetActive(true); // ストップボタンを再表示
@@ -25,6 +55,8 @@ public class GameStop : MonoBehaviour
     // タイトルへ戻るボタンを押したとき
     public void OnRetryNo()
     {
+        stop.clip = closeSound;
+        stop.Play();
         Time.timeScale = 1f; // ポーズ解除
         PlayerStats.Instance.ResetData(); // ステータス初期化
         SceneManager.LoadScene("StartScene"); // タイトルへ遷移
--- Assets/script/TitleController.cs
+++ Assets/script/TitleController.cs
@@ -1,11 +1,36 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleController : MonoBehaviour
 {
-    // startボタンを押したら、シーンを切り替る
+    private AudioSource start; // AudioSource
+    public AudioClip startSound;   // 最初の音
+    public AudioClip startSound2;  // 次の音
+
+    private void Awake()
+    {
+        start = GetComponent<AudioSource>(); // AudioSource を取得
+    }
+
     public void OnStartButtonClicked()
     {
+        // 音を順番に鳴らす（同時再生も可能）
+        if (start != null)
+        {
+            if (startSound != null) start.PlayOneShot(startSound);
+            if (startSound2 != null) start.PlayOneShot(startSound2);
+        }
+        //
+        StartCoroutine(GameStart());
+        
+    }
+
+    IEnumerator GameStart()
+    {
+        yield return new WaitForSeconds(1.5f);
+        // シーン遷移（音を鳴らしてから遷移したいならコルーチンにする）
         SceneManager.LoadScene("SampleScene");
+
     }
 }
