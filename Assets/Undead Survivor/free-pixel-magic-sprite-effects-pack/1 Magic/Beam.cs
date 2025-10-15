2050bf12dc06e9cc4ef0199696e9cb88a7b05130
wakimoto <sfom00122@gmail.com>
2025-10-15 16:24:38 +0900

    確認

--- Assets/Undead Survivor/free-pixel-magic-sprite-effects-pack/1 Magic/Beam.cs
+++ Assets/Undead Survivor/free-pixel-magic-sprite-effects-pack/1 Magic/Beam.cs
@@ -1,15 +1,20 @@
 using UnityEngine;
-using DG.Tweening; // DOTweenを使用するために必要
+using DG.Tweening;
 
 public class Beam : MonoBehaviour
 {
     void Start()
     {
-        // DORotateメソッドでZ軸の回転アニメーションを設定
+        // 1秒かけて現在の状態からZ軸を-180度（時計回り）回転させる
         transform.DORotate(
-            new Vector3(0, 0, -360), // 
-            2f // アニメーションにかける時間（1秒）
+            new Vector3(0, 0, -180), // Z軸に-180度（時計回り）
+            1f // アニメーションにかける時間
         )
-        .SetEase(Ease.InOutQuad); // 加速するイージングタイプを設定
+        .SetEase(Ease.InOutQuad)
+        .SetRelative(true)
+        .OnComplete(() => {
+            // アニメーション完了後にこのゲームオブジェクトを破棄する
+            Destroy(gameObject);
+        });
     }
 }
